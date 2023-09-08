6b5c68e773bf03d02354d21b590b2a518cb26d6b
tharris <17254429+tharris104@users.noreply.github.com>
2023-09-07 22:56:23 -0400

    autoupdate

--- resources/[extra]/StrictPolice/client.lua
+++ resources/[extra]/StrictPolice/client.lua
@@ -48,6 +48,7 @@ function IsPlayerInPedFOV(ped, player, fovAngle)
         local playerCoords = GetEntityCoords(player, false)
         local pedForwardVector = GetEntityForwardVector(ped)
 
+
         local directionToPlayer = playerCoords - pedCoords
         directionToPlayer = directionToPlayer / #(directionToPlayer) -- Normalize the vector
 
@@ -56,9 +57,10 @@ function IsPlayerInPedFOV(ped, player, fovAngle)
         if math.abs(angle) <= fovAngle then
                 if debug_enabled then
                         print('IsPlayerInPedFOV() - Player in PED angle (' .. angle .. ' <= ' .. fovAngle .. ')')
+                        print('IsPlayerInPedFOV() - Starting shape test with parameters (' .. pedCoords.x .. ',' .. pedCoords.y .. ',' .. pedCoords.z + 1.0 .. ',' .. playerCoords.x .. ',' .. playerCoords.y .. ',' .. playerCoords.z + 1.0 .. ',' .. ped .. ')')
                 end
                 -- Start shape test against all relevant flags (-1) https://docs.fivem.net/natives/?_0x7EE9F5D83DD4F90E
-                local rayHandle = StartShapeTestLosProbe(
+                local rayHandle = StartExpensiveSynchronousShapeTestLosProbe(
                         pedCoords.x,
                         pedCoords.y,
                         pedCoords.z + 1.0,
@@ -70,7 +72,7 @@ function IsPlayerInPedFOV(ped, player, fovAngle)
                         4
                 )
                 local result = -1
-                local retval, hit, endCoords, surfaceNormal, materialHash, entityHit -- Add local declarations here
+                local retval, hit, endCoords, surfaceNormal, entityHit -- Add local declarations here
 
                 local timeoutCounter = 0
                 local timeoutThreshold = 100 -- Adjust this value as needed
@@ -78,7 +80,7 @@ function IsPlayerInPedFOV(ped, player, fovAngle)
                 while result == -1 and timeoutCounter < timeoutThreshold do
                         Citizen.Wait(0) -- Yield to the game's main loop
                         --retval, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(rayHandle)
-                        retval, hit, endCoords, surfaceNormal, materialHash, entityHit = GetShapeTestResultIncludingMaterial(rayHandle)
+                        retval, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(rayHandle)
                         timeoutCounter = timeoutCounter + 1
                 end
 
