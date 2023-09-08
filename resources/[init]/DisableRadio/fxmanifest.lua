1723e708abf371e091d72809a7f24def87ba1ce6
tharris <17254429+tharris104@users.noreply.github.com>
2023-09-08 07:18:14 -0400

    added some more emojis to load screen

--- resources/[init]/DisableRadio/fxmanifest.lua
+++ resources/[init]/DisableRadio/fxmanifest.lua
@@ -2,10 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'nemesis104'
-description 'Loop and disabling radio.. needs improvement'
+description 'Loop and disable radio constantly.. tired of turning it off while testing'
 version '1.0.0'
 
-
 client_script {
-'client.lua',
+    'client.lua',
 }
