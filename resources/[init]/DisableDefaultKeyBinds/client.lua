17b22e30d1bff432391fa709eff40e91bb6e329a
tharris <17254429+tharris104@users.noreply.github.com>
2023-09-09 19:15:30 -0400

    new mod to disable default key binds

--- resources/[init]/DisableDefaultKeyBinds/client.lua
+++ resources/[init]/DisableDefaultKeyBinds/client.lua
@@ -2,7 +2,14 @@
 local controlCleared = false
 
 -- Unset the control hash 116 (currently used by simpleCarHUD)
-if not controlCleared then
-  N_0x06462a961e94b67c(GetHashKey("INPUT_VEH_SELECT_PREV_WEAPON")) -- https://docs.fivem.net/natives/?_0x06462A961E94B67C
-  controlCleared = true
-end
+-- Main thread checking if player is in a bounty area
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if not controlCleared then
+      N_0x06462a961e94b67c(GetHashKey("INPUT_VEH_SELECT_PREV_WEAPON")) -- https://docs.fivem.net/natives/?_0x06462A961E94B67C
+      print('cleared keybind for INPUT_VEH_SELECT_PREV_WEAPON')
+      controlCleared = true
+    end
+  end
+end)
