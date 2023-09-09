51c88e91f293551284df7c6342b7d9775f151542
tharris <17254429+tharris104@users.noreply.github.com>
2023-09-09 19:30:41 -0400

    autoupdate

--- resources/[init]/DisableDefaultKeyBinds/client.lua
+++ resources/[init]/DisableDefaultKeyBinds/client.lua
@@ -1,14 +1,21 @@
 -- Unset default built-in controls for FiveM --
 local controlCleared = false
-
+local controlKeyName = "INPUT_VEH_FLY_SELECT_PREV_WEAPON" -- 116
+local controlKeyHash = GetHashKey(controlKeyName)
+local replaceKeyName = "replace_with_something" -- https://docs.fivem.net/docs/game-references/controls/#controls
+local replaceKeyHash = GetHashKey(replaceKeyName)
 -- Unset the control hash 116 (currently used by simpleCarHUD)
 -- Main thread checking if player is in a bounty area
 Citizen.CreateThread(function()
   while true do
-    Citizen.Wait(0)
+    Citizen.Wait(5000) -- check every 5 seconds
     if not controlCleared then
-      N_0x06462a961e94b67c(GetHashKey("INPUT_VEH_SELECT_PREV_WEAPON")) -- https://docs.fivem.net/natives/?_0x06462A961E94B67C
+      N_0x06462a961e94b67c(controlKeyHash) -- https://docs.fivem.net/natives/?_0x06462A961E94B67C
       print('cleared keybind for INPUT_VEH_SELECT_PREV_WEAPON')
+
+      -- Disabling this - not sure if its even working or not
+      -- N_0x5b73c77d9eb66e24(controlKeyHash, replaceKeyHash)
+
       controlCleared = true
     end
   end
