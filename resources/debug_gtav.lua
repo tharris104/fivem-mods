def7e27bf4ce2938ceaacb690d760158f44fbf55
tharris104 <17254429+tharris104@users.noreply.github.com>
2023-11-16 19:46:00 -0500

    dont bother

--- resources/debug_gtav.lua
+++ resources/debug_gtav.lua
@@ -1,11 +1,11 @@
--- Function to load a script
-function loadScript(scriptName)
-  print("Loading script: " .. scriptName)
-  dofile(scriptName)
-end
-
--- load fivem natives
-loadScript("fivem_natives.lua")
-
--- load script to test
-loadScript("[extra]/StrictPolice/client.lua")
+---- Function to load a script... dont bother
+--function loadScript(scriptName)
+--  print("Loading script: " .. scriptName)
+--  dofile(scriptName)
+--end
+--
+---- load fivem natives
+--loadScript("fivem_natives.lua")
+--
+---- load script to test
+--loadScript("[extra]/StrictPolice/client.lua")
